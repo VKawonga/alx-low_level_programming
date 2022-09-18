27ab516835dc66b1b34ce0c69d7a313d943a51be
VKawonga <vkawonga@sbz.com.zm>
2022-09-18 09:41:11 -0700

    Fourth project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/** printf - prints "with proper grammar, but the outcome is a piece of art,"
+/** printf - prints specified string
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art," using a print f function
+ * Description: prints "with proper grammar, but the outcome is a piece of art," 
  * Return: "with proper grammar, but the outcome is a piece of art,"
  */
-
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
