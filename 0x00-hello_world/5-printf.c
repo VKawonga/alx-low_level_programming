c7fd4f03eab0d1219ee740e6b806bb029a153638
VKawonga <vkawonga@sbz.com.zm>
2022-09-18 09:56:04 -0700

    Fourth project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /** printf - prints specified string
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art," 
+ * Description:prts"with proper grammar, but the outcome is a piece of art,"
  * Return: "with proper grammar, but the outcome is a piece of art,"
  */
 int main(void)
