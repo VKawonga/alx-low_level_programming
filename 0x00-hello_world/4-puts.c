18201e7e3b4408de49c432042f4e692cd2fef013
VKawonga <vkawonga@sbz.com.zm>
2022-09-18 07:30:49 -0700

    Fourth project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
+/*prints "Programming is like building a multilingual puzzle without using printf */
 
-	int main(void) {
-
-		char mystr[] = "Programming is like building a multilingual puzzle";
-	        
-		puts(mystr);
-
-		return(0);
+int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle\n";        
+	puts(mystr);
+	return(0);
 }
 
