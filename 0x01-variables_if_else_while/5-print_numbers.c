1f606faf300fe9f50f245cc958e3b2126e5a4426
VKawonga <vkawonga@sbz.com.zm>
2022-09-20 09:58:30 -0700

    Fifth project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all digits
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int a = 0;
+
+	printf("%d", a);
+	a++;
+	return (0);
+}
