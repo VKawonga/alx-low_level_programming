e3ce63ab7ecb9c9add5920bf7d7727aa66f0e223
VKawonga <vkawonga@sbz.com.zm>
2022-09-20 08:11:50 -0700

    Fifth project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet except q and e
+ *
+ * Description: Prints the alphabet except q
+ * and e using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int ch = 'a';
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q' || 'e')
+		{
+		putchar(ch);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
