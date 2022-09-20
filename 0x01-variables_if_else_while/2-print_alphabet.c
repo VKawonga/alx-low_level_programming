56283929315f49c02b2fe3d130380a581c4aa2be
VKawonga <vkawonga@sbz.com.zm>
2022-09-20 07:08:48 -0700

    Fifth project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Description: Prints the alphabet using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+
+{	
+	for (int ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		int putchar(int ch);
+	}
+
+        putchar ('\n');
+
+	return (0);
+
+}
