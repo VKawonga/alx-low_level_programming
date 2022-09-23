ee6ed399508a5a64530f8001fd6c4e386b47f90d
VKawonga <vkawonga@sbz.com.zm>
2022-09-23 05:34:18 -0700

    Sixth commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/** main.h contains the necessary header files as well 
+ * as the prototypes for the various functions to be created
+ *  in projects
+ */
+
 int _putchar(char c)
 void print_alphabet(void);
