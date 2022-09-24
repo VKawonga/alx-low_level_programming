4e50c8ef4dc49fa5af40f4eef48e743c1c73a03b
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 09:12:56 -0700

    Sixth project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints the last digit of a number
+ * @a: the integer whose last digit is to be printed
+ *
+ * Description - prints the last digit of a number.
+ *
+ * Return: the last digit of an integer.
+ */
+int print_last_digit(int a)
+{
+	int mb;
+
+	mb = a % 10;
+	return (mb);
+}
