a34d7b508059209994411e093b48fe9246a6a84a
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 10:31:00 -0700

    Sixth project

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,31 +9,35 @@
  */
 char jack_bauer(void)
 {
-	int i = '0';
-	int j = '0';
-	int k = '0';
-	int l = '0';
+	int i = '0' , j , k , l;
 
-	while (i <= '9')
+	while (i <= '2')
 	{
+		int j = '0';
 		while (j <= '9')
 		{
-			while (k <= '9')
+			int k = '0';
+			while (k <= '6')
 			{
+				int l = '0';
 				while (l <= '9')
 				{
 					_putchar(l);
+					_putchar('\n');
 					l++;
 				}
 				_putchar(k);
+				_putchar('\n');
 				k++;
 			}
 			_putchar(j);
+			_putchar('\n');
 			j++;
 		}
 		_putchar(i);
+		_putchar('\n');
 		i++;
 	}
 
 	return (0);
-}
+iii}
