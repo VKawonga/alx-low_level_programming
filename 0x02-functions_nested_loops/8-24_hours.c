e7c5d6a73419fca5c2f8883b3efbbf12998bb81c
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 12:01:26 -0700

    Sixth project

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ char jack_bauer(void)
 	{
 		for (j = '0' ; j <= '9' ; j++)
 		{
-			for (k = '0' ; k <= '6' ; k++)
+			for (k = '0' ; k < '6' ; k++)
 			{
 				for (l = '0' ; l <= '9' ; l++)
 				{
