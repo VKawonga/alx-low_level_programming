9aef8ab8b594e9a1406a86d30f327b87764f62ed
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 02:30:20 -0700

    Sixth project

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
  * _islower - prints whether or not a character is lower case or not
- * @c the character to check
+ * @c: the character to check
  *
  * Description -_islower prints whether or not a character is lower case or not
  * using islower function
  *
- * Returns: 1 if c is lower cae and 0 otherwise
+ * Returns - _islower returns 1 if c is lower cae and 0 otherwise
  */
 int _islower(int c)
 {
