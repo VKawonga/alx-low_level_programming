666ed07ab82f20d0f9e53b7634b6edfbe38a62a1
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 05:26:45 -0700

    Sixth project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,6 +12,11 @@
  */
 int print_sign(int n)
 {
+	if ((n < '0'))
+	{
+		_putchar('-');
+		return(-1);
+	}
 	if ((n > '0'))                                                                                                      
 	{
 		_putchar('+');
@@ -20,12 +25,6 @@ int print_sign(int n)
 	if ((n = '0'))
 	{
 		_putchar('0');
-		return (0);
-	}
-	if ((n < '0'))
-	{
-		_putchar('-');
-		_putchar('1');
 		return (0);
 	}
 
