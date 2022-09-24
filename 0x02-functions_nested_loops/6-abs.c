f39efb91389850c683504851947897cf2f69a582
VKawonga <vkawonga@sbz.com.zm>
2022-09-24 08:43:49 -0700

    Sixth project

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * Description - computes the absolute value of an integer.
+ *
+ * Return: 0
+ */
+int _abs(int);
+{
+	abs();
+	return (0)
+}
