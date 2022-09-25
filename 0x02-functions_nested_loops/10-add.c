63df548ff1cb8ff7511fac70f2e4627363b6aeeb
VKawonga <vkawonga@sbz.com.zm>
2022-09-25 03:41:25 -0700

    Sixth project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * Description - adds two integers and returns the result.
+ *
+ * Return: sum of two integers
+ */
+int add(int, int)
+{
+	int sum;
+
+	sum = int + int;
+	return (sum);
+}
