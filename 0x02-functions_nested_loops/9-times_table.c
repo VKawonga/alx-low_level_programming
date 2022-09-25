bbbea31523b47332fa702335f1a77554b0399b83
VKawonga <vkawonga@sbz.com.zm>
2022-09-25 03:28:08 -0700

    Sixth project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ char times_table(void)
 {
 	char i, j;
 
-	for (i == 0 ; i <= 9 ; i++)
+	for (i = '0' ; i <= '9' ; i++)
 	{
-		for (j == 0 ; j <= 9 ; j++)
+		for (j = '0' ; j <= '9' ; j++)
 		{
 			_putchar(i * j);
 			_putchar(',');
